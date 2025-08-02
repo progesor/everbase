88fb109778cb2b7cf671b90342e10e3e2235e550
Catlak Profesor <progesor@gmail.com>
2025-08-02 20:50:54 +0300

    chore(web): move api url to .env file
    Moves the hardcoded API_BASE_URL to an environment variable (VITE_API_BASE_URL) managed by a .env.local file. Close #15

--- apps/aether-web/src/lib/api.ts
+++ apps/aether-web/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import {ContainerInfo} from "@everbase/types";
 
 // Geliştirme ortamında backend'imizin çalıştığı adresi belirtiyoruz.
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
